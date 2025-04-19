d0820ed0fd22461a018f15212b3fd1d34bb24bb6
ipilla <money78@gmail.com>
2025-04-19 18:21:18 +0200

    s

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,27 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import commonjs        from '@rollup/plugin-commonjs';
+import { terser }      from 'rollup-plugin-terser';
 
 export default {
   input: 'src/bubble-room.js',
   output: {
-    file: 'lovelace-bubble-room.js',        // Genera il file bundle nella cartella "dist"
+    file: 'lovelace-bubble-room.js',  // singolo file
     format: 'esm',
-    inlineDynamicImports: true
+    inlineDynamicImports: true       // <— serve per "inserire" i dynamic import nello stesso bundle
   },
   external: [
+    // HA components
     'home-assistant-frontend/src/components/ha-entity-picker.js',
     'home-assistant-frontend/src/components/ha-expansion-panel.js',
+    // lit & directive
+    'lit',
+    'lit/directives/style-map.js',
+    // fitty
+    'fitty',
   ],
   plugins: [
     nodeResolve(),
     commonjs(),
-    terser()
+    terser(),
   ]
 };
