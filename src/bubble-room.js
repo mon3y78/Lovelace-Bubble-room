d9f9140cb21c73f417cb001caf0a0750447ccf9a
ipilla <money78@gmail.com>
2025-05-24 17:04:46 +0200

    d

--- src/bubble-room.js
+++ src/bubble-room.js
@@ -610,7 +610,7 @@ class BubbleRoom extends LitElement {
       '12x4': {
         cardHeight: '250px',
         iconSize: '130px',
-        iconTop: '-20%',
+        iconTop: '0%',
         iconLeft: '2%',
         nameFont: '32px',
         nameTop: '12px',
