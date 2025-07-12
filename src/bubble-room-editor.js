09f4d0032f96b4bd323637c60245f5ad0d700222
mon3y78 <money78@gmail.com>
2025-07-12 11:39:30 +0200

    update bubble-room-editor.js

--- src/bubble-room-editor.js
+++ src/bubble-room-editor.js
@@ -1338,8 +1338,8 @@ class BubbleRoomEditor extends LitElement {
                     </ha-icon-picker>
                   </div>
                   <div style="display: flex; flex-direction: row; gap: 24px; flex-wrap: wrap; margin-top: 8px;">
-                    ${this._renderTapHoldAction(key)}
-                    ${this._renderTapHoldAction(key)}
+                    ${this._renderTapHoldAction("tap")}
+                    ${this._renderTapHoldAction("hold")}
                   </div>
                 </div>
               </div>
