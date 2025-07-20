e3d09956bc4e3f28907abfbaa451b8d361fda96b
mon3y78 <money78@gmail.com>
2025-07-20 18:34:39 +0200

    update BubbleIcon.js

--- src/components/BubbleIcon.js
+++ src/components/BubbleIcon.js
@@ -1,16 +1,38 @@
 /**
  * BubbleIcon.js
  * Componente per la visualizzazione dell’icona principale della stanza nella Bubble Room card.
- * Autore: mon3y78 (https://github.com/mon3y78)
+ * -----------------------------------------------------------
+ * - Visualizza un’icona principale ridimensionabile dinamicamente in base allo spazio disponibile.
+ * - Usa la funzione centralizzata getScaledIconSize da utils.js per il calcolo della dimensione.
+ * - Supporta colore di sfondo e colore icona configurabili.
+ * - Gestisce azioni tap/hold centralizzate usando helpers/utils.js.
+ * - Layout responsive e pulito.
+ * 
+ * PROPRIETÀ:
+ *  - icon: String          (Icona mdi da visualizzare)
+ *  - color: String         (Colore di sfondo e icona)
+ *  - containerWidth: Number (Larghezza disponibile per il componente)
+ *  - containerHeight: Number (Altezza disponibile per il componente)
+ *  - config: Object        (Configurazione con tap_action e hold_action)
+ *  - hass: Object          (Home Assistant, necessario per azioni)
+ * 
+ * AUTORE:
+ *  - mon3y78 - https://github.com/mon3y78/Lovelace-Bubble-room
+ * 
+ * LICENZA: MIT
  */
 
 import { LitElement, html, css } from 'lit';
+import { getScaledIconSize, onPointerDown, onPointerUp, onPointerLeave, doTapAction, doHoldAction } from '../helpers/utils.js';
 
 export class BubbleIcon extends LitElement {
   static properties = {
     icon: { type: String },
     color: { type: String },
-    size: { type: Number }
+    containerWidth: { type: Number },
+    containerHeight: { type: Number },
+    config: { type: Object },
+    hass: { type: Object },
   };
   
   static styles = css`
@@ -22,16 +44,19 @@ export class BubbleIcon extends LitElement {
       align-items: center;
       justify-content: center;
       border-radius: 50%;
-      width: var(--icon-size, 80px);
-      height: var(--icon-size, 80px);
       background: var(--icon-bg, transparent);
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
     }
     .icon-inner {
-      font-size: var(--icon-inner-size, 64px);
       color: var(--icon-color, inherit);
       width: 100%;
       height: 100%;
       text-align: center;
+      display: block;
+      user-select: none;
+      pointer-events: none;
     }
   `;
   
@@ -39,15 +64,46 @@ export class BubbleIcon extends LitElement {
     super();
     this.icon = '';
     this.color = '';
-    this.size = 80;
+    this.containerWidth = 80;
+    this.containerHeight = 80;
+    this.config = {};
+    this.hass = null;
   }
   
   render() {
+    const iconSize = getScaledIconSize(this.containerWidth, this.containerHeight, 0.8);
     return html`
-      <div class="bubble-icon" style="width:${this.size}px; height:${this.size}px; background:${this.color || 'transparent'};">
-        <ha-icon class="icon-inner" .icon=${this.icon} style="font-size:${Math.round(this.size * 0.8)}px;"></ha-icon>
+      <div
+        class="bubble-icon"
+        style="background:${this.color || 'transparent'};"
+        @pointerdown=${this._onPointerDown}
+        @pointerup=${this._onPointerUp}
+        @pointerleave=${this._onPointerLeave}
+      >
+        <ha-icon
+          class="icon-inner"
+          .icon=${this.icon}
+          style="font-size:${iconSize}px; color:${this.color || 'inherit'};"
+        ></ha-icon>
       </div>
     `;
+  }
+  
+  _onPointerDown(e) {
+    onPointerDown(e, this.config, this.hass, this._onTap.bind(this), this._onHold.bind(this));
+  }
+  _onPointerUp(e) {
+    onPointerUp(e, this.config, this.hass, this._onTap.bind(this), this._onHold.bind(this));
+  }
+  _onPointerLeave(e) {
+    onPointerLeave();
+  }
+  
+  _onTap(conf, hass) {
+    doTapAction(conf, hass);
+  }
+  _onHold(conf, hass) {
+    doHoldAction(conf, hass);
   }
 }
 
