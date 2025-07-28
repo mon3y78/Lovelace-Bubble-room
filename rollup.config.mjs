8143158c3f905943aa3dc2c00994def96fb7e8c2
mon3y78 <money78@gmail.com>
2025-07-28 22:41:44 +0200

    update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs      from '@rollup/plugin-commonjs';
-
+import json          from '@rollup/plugin-json';
 import { builtinModules } from 'module';
 
 export default {
@@ -9,6 +9,7 @@ export default {
     file: 'lovelace-bubble-room.js',
     format: 'esm',
     inlineDynamicImports: true,
+    sourcemap: true,
   },
   external: id => {
     // 1) Componenti HASS già globali
@@ -16,10 +17,10 @@ export default {
     // 2) @material/mwc-icon
     if (id === '@material/mwc-icon') return true;
     // 3) Tutti i built‑in di Node
-    if (builtinModules.includes(id)) return true;
     return false;
   },
   plugins: [
+    json(),
     nodeResolve({ browser: true, preferBuiltins: false }),
     commonjs(),
   ],
