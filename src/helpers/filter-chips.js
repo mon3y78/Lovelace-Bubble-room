affb6a1d5aac5b42b859300e031bf617a3d087d3
mon3y78 <money78@gmail.com>
2025-07-31 18:14:47 +0200

    update filter-chips.js

--- src/helpers/filter-chips.js
+++ src/helpers/filter-chips.js
@@ -1,51 +1,58 @@
 // src/helpers/filter-chips.js
 import { LitElement, html, css } from 'lit';
+import '@material/mwc-chip';
+import '@material/mwc-chip-set';
 
-/* usa i chip “ufficiali” di HA, già presenti nel bundle */
 import { FILTER_LABELS } from './entity-filters.js';
 
+/**
+ * <filter-chips>
+ *  • value   = array di categorie attive
+ *  • allowed = array di tutte le categorie possibili
+ *  Emesso: "value-changed" con detail.value = nuovo array.
+ */
 export class FilterChips extends LitElement {
   static properties = {
-    value: { type: Array },
+    value:   { type: Array },
     allowed: { type: Array },
   };
-  
+
   constructor() {
     super();
-    this.value = [];
+    this.value   = [];
     this.allowed = [];
   }
-  
+
   static styles = css`
-    ha-chip {
+    mwc-chip {
       margin: 4px;
     }
   `;
-  
+
   _toggle(cat) {
-    const s = new Set(this.value);
-    s.has(cat) ? s.delete(cat) : s.add(cat);
+    const set = new Set(this.value);
+    set.has(cat) ? set.delete(cat) : set.add(cat);
     this.dispatchEvent(new CustomEvent('value-changed', {
-      detail: { value: Array.from(s) },
+      detail: { value: Array.from(set) },
       bubbles: true,
       composed: true,
     }));
   }
-  
+
   render() {
     if (!this.allowed?.length) {
       return html``;
     }
     return html`
-      <ha-chip-set choice>
+      <mwc-chip-set choice>
         ${this.allowed.map(cat => html`
-          <ha-chip
+          <mwc-chip
             .label=${FILTER_LABELS[cat] ?? cat}
             ?selected=${this.value.includes(cat)}
             @click=${() => this._toggle(cat)}
-          ></ha-chip>
+          ></mwc-chip>
         `)}
-      </ha-chip-set>
+      </mwc-chip-set>
     `;
   }
 }
