e6ea0eaf92892b4544d69a35adf082cc749a2090
mon3y78 <money78@gmail.com>
2025-08-05 00:43:15 +0200

    update BubbleSensor.js

--- src/components/BubbleSensor.js
+++ src/components/BubbleSensor.js
@@ -64,7 +64,7 @@ export class BubbleSensor extends LitElement {
     if (maxSize <= 0) return;
 
     element.style.fontSize = '';
-    let fontSize = parseInt(getComputedStyle(element).fontSize, 10) || 14;
+    let fontSize = parseInt(getComputedStyle(element).fontSize, 20) || 14;
 
     while (fontSize > 8) {
       element.style.fontSize = `${fontSize}px`;
