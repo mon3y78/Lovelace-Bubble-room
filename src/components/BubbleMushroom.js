727e2083c8e8a1f94f746d8efe5372a05115c1fe
ipilla <money78@gmail.com>
2025-08-07 13:48:11 +0200

    j

--- src/components/BubbleMushroom.js
+++ src/components/BubbleMushroom.js
@@ -74,8 +74,8 @@ export class BubbleMushroom extends LitElement {
 
     /* coefficiente k in funzione del viewport */
     const vp        = window.innerWidth;
-    const kMobile   = 0.9;
-    const kDesktop  = 0.5;
+    const kMobile   = 0.6;
+    const kDesktop  = 0.3;
     const wMobile   = 100;
     const wDesktop  = 200;
 
